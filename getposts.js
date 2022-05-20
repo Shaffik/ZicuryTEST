91dab64524174410f82c5bf39cf6dbd212a79e93
Shaffik <ddraig.gotch@gmail.com>
2022-05-20 11:02:34 -0500

    Add files via upload

---
+++ getposts.js
@@ -0,0 +1,16 @@
+const base = 'https://gorest.co.in/public/v2/'; //declaramos la url base de las consultas
+global.XMLHttpRequest = require('xhr2'); // se declara xhr en todo el  entorno
+var xhr = new XMLHttpRequest(); //se declara xhr como una variable para realizar peticiones xhr
+
+xhr.onload = function(){// Procesa la respuesta después de terminar la consulta
+    if(xhr.status>=200 && xhr.status<300){ //Procesamos y validamos la respuesta del servidor 200 ok
+        console.log(xhr.responseURL)
+        console.log(xhr.status, 'Petición existosa'); //Lo que se hace con la respuesta del server
+    }else{
+        console.log(xhr.responseURL)
+        console.log(xhr.status, 'Ha ocurrido un Error, Petición fallida');
+    }
+};
+
+xhr.open('GET', `${base}posts`);//especifica la ruta de acceso y el método
+xhr.send(null);// Se encarga de encviar la petición
